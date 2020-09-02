4cfc0d8c335787d5a4d5988b624c9a8fb1d3f305
MiroslavGannoha <miroslav.gannoha@gmail.com>
2020-09-02 15:00:59 +0300

    react-testing-library added

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     clearMocks: true,
-    testEnvironment: 'node',
-    snapshotSerializers: ['enzyme-to-json/serializer'],
-    setupFiles: ['./test/setupTests.ts'],
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['./test/setupAfterEnv.ts'],
 };
--- src/app/pages/Dashboard/index.test.tsx
+++ src/app/pages/Dashboard/index.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Dashboard from './';
-import { shallow } from 'enzyme';
+// import { shallow } from 'enzyme';
+import {render} from '@testing-library/react';
 
 it('should render correctly', () => {
-    const component = shallow(<Dashboard />);
+    const {getByText} = render(<Dashboard />);
 
-    expect(component).toMatchSnapshot();
+    expect(getByText('Dashboard')).toBeVisible();
 });
