86a42b86be8de1d2bb98ba77d3a855b904abd9a3
MiroslavGannoha <miroslav.gannoha@gmail.com>
2020-09-05 21:04:14 +0300

    testing setup improvements

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,3 +2,10 @@ declare module '*.json' {
     const value: unknown;
     export default value;
 }
+
+declare namespace jest {
+    interface Matchers<R> {
+        toHaveFetched: (expected: string) => CustomMatcherResult;
+        toHaveFetchedTimes: (n: number, expected?: string) => CustomMatcherResult;
+    }
+}
