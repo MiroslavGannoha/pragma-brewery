d5e42823b50e0e33067304aaf14d961d73b10eae
MiroslavGannoha <miroslav.gannoha@gmail.com>
2020-09-07 19:38:36 +0300

    svg and file loader config

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,6 +3,11 @@ declare module '*.json' {
     export default value;
 }
 
+declare module '*.svg' {
+    const value: string;
+    export default value;
+}
+
 declare namespace jest {
     interface Matchers<R> {
         toHaveFetched: (expected: string) => CustomMatcherResult;
--- webpack.config.ts
+++ webpack.config.ts
@@ -74,7 +74,15 @@ export default {
 						}
 					}
 				]
-			}
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    // outputPath: 'assets/',
+                },
+            },
 		]
 	},
 
